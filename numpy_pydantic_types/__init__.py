f19dcd5d15a72411841e34580c929df829cc8fbd
Álvaro Vázquez Ortiz <alvvazort@gmail.com>
2024-11-17 12:44:29 +0100

    feat: created public interface

--- numpy_pydantic_types/__init__.py
+++ numpy_pydantic_types/__init__.py
@@ -1,21 +1,9 @@
 from types import new_class
-from typing import TYPE_CHECKING
 from numpy import float16 as np_float16
 from numpy import float32 as np_float32
 from numpy import float64 as np_float64
 
-from .floats import NumpyFloat  # Adjust import path as needed
-
-__all__ = ["float16", "float32", "float64"]
-
-if TYPE_CHECKING:
-
-    class float16(np_float16, NumpyFloat[np_float16]): ...
-
-    class float32(np_float32, NumpyFloat[np_float32]): ...
-
-    class float64(np_float32, NumpyFloat[np_float64]): ...
-
+from .floats import NumpyFloat
 
 # Dynamically create each float class at runtime
 for name, np_type in [
